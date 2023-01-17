b03b39e0b66c4c20a54ac0087c108163883f20fa
ReynaldJr <reynaldpadillajr17@gmail.com>
2023-01-17 10:37:58 +0400

    Add splash screen

---
+++ app/src/main/java/com/example/solarsystem/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.solarsystem
+
+import android.app.ActivityOptions
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Pair
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.top_logo)
+        val textAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_logo)
+        logo_image.startAnimation(logoAnimation)
+        logo_text.startAnimation(textAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
